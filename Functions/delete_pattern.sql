8b81a06ba224c1a98ff19ad284806c7b0076bdda
Ben Rathbone <rath3.14159@protonmail.com>
2022-01-17 10:36:06 -0600

    Delete Sproc

--- Functions/delete_pattern.sql
+++ Functions/delete_pattern.sql
@@ -6,6 +6,7 @@ RETURNS VOID
 LANGUAGE plpgsql AS 
 $$
 BEGIN
+  DELETE FROM patterns_schema.pattern_generation CASCADE WHERE pattern_uuid = _pattern_uuid;
   DELETE FROM patterns_schema.pattern_list CASCADE WHERE pattern_uuid = _pattern_uuid;
 END
 $$;
